3a75edd19a8973338df996547faa085bc4202672
leticia-UTFPR <leticiamarchioni@alunos.utfpr.edu.br>
2025-06-05 23:18:35 -0300

    Arquivo de Produto

---
+++ ecommerce-projeto/src/models/Produto.js
@@ -0,0 +1,58 @@
+const { connect } = require("../db");
+const Logger = require("../logger");
+
+class Produto {
+  constructor(nome, descricao, categoria, preco, estoque) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.categoria = categoria;
+    this.preco = preco;
+    this.estoque = estoque;
+  }
+
+  static async inserir(dados) {
+    try {
+      const { db, client } = await connect();
+      const result = await db.collection("produtos").insertOne(dados);
+      console.log("Produto inserido:", result.insertedId);
+      client.close();
+    } catch (error) {
+      Logger.log("Erro ao inserir produto: " + error);
+    }
+  }
+
+  static async buscar(filtro = {}) {
+    try {
+      const { db, client } = await connect();
+      const produtos = await db.collection("produtos").find(filtro).toArray();
+      console.log("Produtos encontrados:", produtos);
+      client.close();
+    } catch (error) {
+      Logger.log("Erro ao buscar produtos: " + error);
+    }
+  }
+
+  static async atualizar(filtro, novosDados) {
+    try {
+      const { db, client } = await connect();
+      const result = await db.collection("produtos").updateMany(filtro, { $set: novosDados });
+      console.log("Produtos atualizados:", result.modifiedCount);
+      client.close();
+    } catch (error) {
+      Logger.log("Erro ao atualizar produtos: " + error);
+    }
+  }
+
+  static async deletar(filtro) {
+    try {
+      const { db, client } = await connect();
+      const result = await db.collection("produtos").deleteMany(filtro);
+      console.log("Produtos deletados:", result.deletedCount);
+      client.close();
+    } catch (error) {
+      Logger.log("Erro ao deletar produtos: " + error);
+    }
+  }
+}
+
+module.exports = Produto;
